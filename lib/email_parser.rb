408410d4e9319b296556b8a3aa887ecb6f50b4eb
Jason J Cruz <jaycru06@yahoo.com>
2019-12-08 22:09:44 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailAddressParser
   
    def parse 
      email_addresses.split.collect do |email|
-       email.gsub(/\s+/, "")
+       email.gsub(/[,\s+]/, "")
      end.flatten.uniq
    end 
   
